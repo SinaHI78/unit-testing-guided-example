061659388ce32bf68420ecc478c90fa0d2e8224f
Sina Joos <sinajoos@gmx.de>
2022-01-22 14:01:37 +0100

    11 tests passed

--- calculator.js
+++ calculator.js
@@ -1,17 +1,49 @@
 function sum(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  } else if (!a && b) {
+    return b;
+  } else if (a && !b) {
+    return a;
+  } else {
+    return a + b;
+  }
 }
 
 function subtract(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  } else if (!a && b) {
+    return b;
+  } else if (a && !b) {
+    return a;
+  } else {
+    return a - b;
+  }
 }
 
 function divide(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  } else if (!a && b) {
+    return error;
+  } else if (a && !b) {
+    return error;
+  } else {
+    return a / b;
+  }
 }
 
 function multiply(a, b) {
-  return a * b;
+  if (!a && !b) {
+    return 0;
+  } else if (!a && b) {
+    return b;
+  } else if (a && !b) {
+    return a;
+  } else {
+    return a * b;
+  }
 }
 
 // The following is required to make unit tests work. Please ignore it.
